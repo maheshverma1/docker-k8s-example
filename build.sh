2ae53544bd7f672839cb0e4a682c793e5f86c9fb
Mahesh Kumar <maheshverma1509@gmail.com>
2019-12-15 13:33:32 +0530

    added travis script

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #echo "mvn clean install"
 #mvn clean install
 
-
-docker build -t maheshkumarctl/docker-k8s-example:latest .
+docker build -t maheshkumarctl/docker-k8s-example:$(git rev-parse HEAD) .
 
 docker login -u maheshkumarctl -p mk@verma1
 
-docker push maheshkumarctl/docker-k8s-example:latest
+docker push maheshkumarctl/docker-k8s-example:$(git rev-parse HEAD)
 
 
 #kubectl apply -f k8s
 #kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
+
